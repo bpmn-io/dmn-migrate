4d82b3a53265877584eab78b5bfadb0a5e17004a
Nico Rehwaldt <git_nikku@nixis.de>
2020-03-05 14:29:49 +0100

    test(integration): add failing 1.1 -> 1.3 migration test

---
+++ test/spec/integrationSpec.js
@@ -0,0 +1,60 @@
+import { expect } from 'chai';
+
+import fs from 'fs';
+
+import { validateXML } from 'xsd-schema-validator';
+
+import { migrateDiagram } from '../../src/migrateDiagram';
+
+const xsd = 'resources/dmn/xsd/DMN13.xsd';
+
+
+function verify(fileName, iit=it) {
+
+  iit(`should migrate <${fileName}>`, async function() {
+
+    // given
+    let xml = read(fileName);
+
+    // when
+    xml = await migrateDiagram(xml);
+
+    const result = await validate(xml);
+
+    // then
+    expect(result.valid).to.be.true;
+  });
+}
+
+verify.only = function(fileName) {
+  verify(fileName, it.only);
+};
+
+
+describe('integration', function() {
+
+  this.timeout(10000);
+
+  verify('test/fixtures/integration/18.dmn');
+
+});
+
+
+
+// helpers //////////
+
+async function validate(xml) {
+  return new Promise((resolve, reject) => {
+    validateXML(xml, xsd, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(result);
+    });
+  });
+}
+
+function read(path, encoding = 'utf8') {
+  return fs.readFileSync(path, encoding);
+}
