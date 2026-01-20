591479e80460a55787f4443eb194e1464e73b5a7
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-20 23:03:59 +0100

    test: improve distro spec

--- test/distro/dmn-migrate.cjs
+++ test/distro/dmn-migrate.cjs
@@ -1,11 +1,19 @@
+const { expect } = require('chai');
+
+
 describe('dmn-migrate', function() {
 
   it('should expose CJS bundle', async function() {
+    const { migrateDiagram } = require('@bpmn-io/dmn-migrate');
 
-    const { expect } = await import('chai');
-
-    const dmnMigrate = require('@bpmn-io/dmn-migrate');
-
-    expect(dmnMigrate.migrateDiagram).to.exist;
+    expect(migrateDiagram).to.exist;
   });
+
+
+  it('should expose ESM bundle', async function() {
+    const { migrateDiagram } = await import('@bpmn-io/dmn-migrate');
+
+    expect(migrateDiagram).to.exist;
+  });
+
 });
